d5b181834a31335b0a2263e9eaacee67b71a12d5
Rishib#321 <rishitab2209@gmail.com>
2025-02-09 14:17:00 +0530

    enabled redis caching in main app.

--- src/main/java/com/springbootredisexample/redisdemo/RedisdemoApplication.java
+++ src/main/java/com/springbootredisexample/redisdemo/RedisdemoApplication.java
@@ -2,8 +2,10 @@ package com.springbootredisexample.redisdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching   // Enables redis caching
 public class RedisdemoApplication {
 
 	public static void main(String[] args) {
