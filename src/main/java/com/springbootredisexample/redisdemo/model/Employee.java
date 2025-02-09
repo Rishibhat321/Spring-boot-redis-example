7b0dc5f1576e81d1f4f723fe1674ec377443c363
Rishib#321 <rishitab2209@gmail.com>
2025-02-09 14:19:59 +0530

    created employee model

--- src/main/java/com/springbootredisexample/redisdemo/model/Employee.java
+++ src/main/java/com/springbootredisexample/redisdemo/model/Employee.java
@@ -1,4 +1,50 @@
 package com.springbootredisexample.redisdemo.model;
 
-public class Employee {
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+
+    // defining fields
+    private int id;
+    private String name;
+    private String department;
+
+    // define constructors
+    public Employee() {
+
+    }
+
+    public Employee(String department, int id, String name) {
+        this.department = department;
+        this.id = id;
+        this.name = name;
+    }
+
+    // define getters/setters
+
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
