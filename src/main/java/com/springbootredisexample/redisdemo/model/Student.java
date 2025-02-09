9544b67a80e4a02eee447f712693e11fc7ffce08
Rishib#321 <rishitab2209@gmail.com>
2025-02-09 21:38:35 +0530

    created Student model

--- src/main/java/com/springbootredisexample/redisdemo/model/Student.java
+++ src/main/java/com/springbootredisexample/redisdemo/model/Student.java
@@ -1,4 +1,87 @@
 package com.springbootredisexample.redisdemo.model;
 
-public class Student {
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+// implementing Serializable for Redis
+
+@Entity
+@Table(name = "student")
+public class Student implements Serializable {
+
+    // define fields
+    // map fields to db columns
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "stud_id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "mobile")
+    private int mobile;
+
+    // define constructors
+    public Student () {}
+
+    public Student(String firstName, String lastName, String email, int mobile) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.mobile = mobile;
+    }
+
+    // define getters/setters
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(int mobile) {
+        this.mobile = mobile;
+    }
+
+
 }
